49c5c2903c821d51f70bbccf1522b5468b267cef
Andrei F <soheevich@gmail.com>
2018-04-11 04:42:58 +0300

    Add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
-const pump = require('pump');
 // const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const mergeStream = require('merge-stream');
+const browserSync = require('browser-sync');
+
+const server = browserSync.create();
 
 
 gulp.task('copy', () => mergeStream(
@@ -17,16 +19,15 @@ gulp.task('copy', () => mergeStream(
 gulp.task('copy-html', () =>
   mergeStream(gulp.src('*.html').pipe(gulp.dest('build/'))));
 
-gulp.task('compress', (cb) => {
-  pump(
-    [gulp.src('src/js/*.js'),
-      babel({ presets: ['env'] }),
-      // uglify({ output: { quote_style: 1 } }),
-      rename({ suffix: '-min' }),
-      gulp.dest('build/js')],
-    cb,
-  );
-});
+// Compile JS
+gulp.task('compress', () =>
+  gulp
+    .src('src/js/*.js', { sourcemaps: true })
+    .pipe(babel({ presets: ['env'] }))
+    // .pipe(uglify({ output: { quote_style: 1 } }))
+    .pipe(rename({ suffix: '-min' }))
+    .pipe(gulp.dest('build/js'))
+    .pipe(server.stream()));
 
 // Compile Sass
 gulp.task('sass', () =>
@@ -36,15 +37,21 @@ gulp.task('sass', () =>
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/styles')));
+    .pipe(gulp.dest('build/styles'))
+    .pipe(server.stream()));
 
 // Watch
-gulp.task('watch', ['sass', 'compress', 'copy-html', 'copy'], () => {
+gulp.task('serve', ['sass', 'compress', 'copy-html', 'copy'], () => {
+  browserSync.init({
+    server: './build',
+  });
+
   gulp.watch(['src/scss/*.scss'], ['sass']);
   gulp.watch(['src/scripts/*.js'], ['compress']);
   gulp.watch(['*.html'], ['copy-html']);
   gulp.watch(['src/images/**/*', 'src/data/*.json'], ['copy']);
+  gulp.watch(['build/*.html']).on('change', server.reload);
 });
 
 // Default
-gulp.task('default', ['watch']);
+gulp.task('default', ['serve']);
